c1fcceb598f7aac8b0da674e82f89137cad74ebe
Luicil <luicilfernandes@hotmail.com>
2021-07-22 14:32:38 -0300

    FrontEnd

---
+++ Produtos/ProdutoFE/Models/ProdutoModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ProdutoFE.Models
+{
+    public class ProdutoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
